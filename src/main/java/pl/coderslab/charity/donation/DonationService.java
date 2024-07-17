8a8413ce3d3cab254277d62c16db87b07f3a9236
Franek051 <franciszektatar@gmail.com>
2024-07-17 14:32:05 +0200

    #2 Akcja home - suma wszystkich oddanych worków

--- src/main/java/pl/coderslab/charity/donation/DonationService.java
+++ src/main/java/pl/coderslab/charity/donation/DonationService.java
@@ -7,13 +7,5 @@ import java.util.List;
 @Service
 public class DonationService {
 
-    private final DonationRepository donationRepository;
 
-    public DonationService(DonationRepository donationRepository) {
-        this.donationRepository = donationRepository;
-    }
-
-    public List<Donation> getDonations() {
-        return donationRepository.findAll();
-    }
 }
