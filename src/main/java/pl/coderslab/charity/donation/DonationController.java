22ee5156d7eb1ba63df7d908f9f2f40e3200e074
Franek051 <franciszektatar@gmail.com>
2024-07-19 14:32:50 +0200

    Formularz

--- src/main/java/pl/coderslab/charity/donation/DonationController.java
+++ src/main/java/pl/coderslab/charity/donation/DonationController.java
@@ -3,10 +3,51 @@ package pl.coderslab.charity.donation;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.category.Category;
+import pl.coderslab.charity.category.CategoryRepository;
+import pl.coderslab.charity.institution.InstitutionRepository;
+import pl.coderslab.charity.donation.Donation;
+import java.util.List;
+
 
 @Controller
 public class DonationController {
+    private final CategoryRepository categoryRepository;
 
+    private final DonationRepository donationRepository;
+
+    private final DonationService donationService;
+
+    private final InstitutionRepository institutionRepository;
+
+    public DonationController(CategoryRepository categoryRepository, DonationRepository donationRepository, DonationService donationService, InstitutionRepository institutionRepository) {
+        this.categoryRepository = categoryRepository;
+        this.donationRepository = donationRepository;
+        this.donationService = donationService;
+        this.institutionRepository = institutionRepository;
+    }
+
+    @GetMapping("/form")
+    public String form(Model model) {
+        model.addAttribute("donation", new Donation());
+        model.addAttribute("categories", categoryRepository.findAll());
+        model.addAttribute("institution", institutionRepository.findAll());
+        return "form";
+    }
+
+    @PostMapping("/form")
+    public String submitForm(@ModelAttribute Donation donation) {
+        donationService.saveDonations(donation);
+        return "redirect:/form-confirmation";
+    }
+
+    @RequestMapping("/form-confirmation")
+    public String confirmForm() {
+        return "confirmation";
+    }
 
 
 
--- src/main/java/pl/coderslab/charity/donation/DonationService.java
+++ src/main/java/pl/coderslab/charity/donation/DonationService.java
@@ -7,5 +7,13 @@ import java.util.List;
 @Service
 public class DonationService {
 
+    private final DonationRepository donationRepository;
 
+    public DonationService(DonationRepository donationRepository) {
+        this.donationRepository = donationRepository;
+    }
+
+    public Donation saveDonations(Donation donation) {
+        return donationRepository.save(donation);
+    }
 }
