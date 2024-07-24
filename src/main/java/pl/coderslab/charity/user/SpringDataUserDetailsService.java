03bdbe09e037e80f6bbda3850e5c77b97bf73abd
Franek051 <franciszektatar@gmail.com>
2024-07-24 14:02:46 +0200

    logowanie

---
+++ src/main/java/pl/coderslab/charity/user/SpringDataUserDetailsService.java
@@ -0,0 +1,27 @@
+package pl.coderslab.charity.user;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+
+@AllArgsConstructor
+@Service
+public class SpringDataUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(), user.getPassword(), new HashSet<>());
+    }
+}
